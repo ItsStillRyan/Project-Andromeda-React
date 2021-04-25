b112bf0ca9e1a30f49c7e1f9dc2fab1617a05008
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-04-25 16:14:49 +0000

    added new footer

--- andromeda-orbital/src/commons/footer.js
+++ andromeda-orbital/src/commons/footer.js
@@ -15,64 +15,9 @@ export default function FooterMain(props) {
             <MDBFooter color="elegant-color-dark" className="font-small pt-4">
                 <MDBContainer fluid className="footerWholeSize ">
                     <MDBRow >
-                        <MDBCol md="6" className="show-col text-center text-md-center">
+                        <MDBCol className="show-col text-center text-md-center">
                             <h4 className="title gitCol">Find This Project on Github</h4>
-                            <a className="gitIcon" href="https://github.com/ItsStillRyan/Project-Pegasus-React"><i className="fab fa-github"></i></a>
-                        </MDBCol>
-
-                        <MDBCol md="3" className="show-col cateList">
-                            <h5 className="title">Categories</h5>
-                            <ul>
-                                <li className="list-unstyled">
-                                    
-                                        <p href={pegasusURL +"/galaxies"}>Galaxies</p>
-                               
-                                </li>
-                                <li className="list-unstyled">
-                                    <Link to="/starcluster">
-                                        <p>Star Clusters</p>
-                                    </Link>
-                                </li>
-                                <li className="list-unstyled">
-                                    <Link to="/planetary">
-                                        <p>Planetary</p>
-                                    </Link>
-                                </li>
-                                <li className="list-unstyled">
-                                    <Link to="/nebulae">
-                                        <p>Nebulae</p>
-                                    </Link>
-                                </li>
-                                <li className="list-unstyled">
-                                    <Link to="/spacecraft">
-                                        <p>Space Craft</p>
-                                    </Link>
-                                </li>
-                                <li className="list-unstyled">
-                                    <Link to="/others">
-                                        <p>Others</p>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </MDBCol>
-
-                        <MDBCol md="3" className="show-col cateList">
-                            <h5 className="title">Help</h5>
-                            <ul>
-                                <li className="list-unstyled">
-                                    <Link to="/gettingstarted">
-                                        <p>Getting Started</p>
-                                    </Link>
-                                </li>
-                            </ul>
-                           <h5 className="title">Uploads</h5>
-                            <ul>
-                                <li className="list-unstyled">
-                                    <Link to="/uploads">
-                                        <p>Upload p picture</p>
-                                    </Link>
-                                </li>
-                            </ul>
+                            <a className="gitIcon" href="https://github.com/ItsStillRyan/Project-Andromeda-React"><i className="fab fa-github"></i></a>
                         </MDBCol>
                     </MDBRow>
                 </MDBContainer>
